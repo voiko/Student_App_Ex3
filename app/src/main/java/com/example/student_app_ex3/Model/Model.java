f99e927b40c3c45769ed4b071792ad7a0ebed161
Voiko <nadavv10@walla.com>
2021-11-27 22:44:49 +0200

    add: StudentListRvActivity myAdapter MyViewHolder class Model class Student

---
+++ app/src/main/java/com/example/student_app_ex3/Model/Model.java
@@ -0,0 +1,25 @@
+package com.example.student_app_ex3.Model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Model {
+    public static final Model instance = new Model();
+
+    private Model(){
+        for(int i=0;i<100;i++){
+            Student s = new Student("name",""+i,false);
+            data.add(s);
+        }
+    }
+
+    List<Student> data = new LinkedList<Student>();
+
+    public List<Student> getAllStudents(){
+        return data;
+    }
+
+    public void addStudent(Student student){
+        data.add(student);
+    }
+}
---
+++ app/src/main/java/com/example/student_app_ex3/StudentListRvAcivity.java
@@ -0,0 +1,96 @@
+package com.example.student_app_ex3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.List;
+import com.example.student_app_ex3.Model.Model;
+import com.example.student_app_ex3.Model.Student;
+
+public class StudentListRvAcivity extends AppCompatActivity {
+
+    List<Student> data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_list_rv_acivity);
+
+        data = Model.instance.getAllStudents();
+
+        RecyclerView list = findViewById(R.id.studentlist_rv);
+        list.setHasFixedSize(true);
+
+        list.setLayoutManager(new LinearLayoutManager(this));
+
+        MyAdapter adapter = new MyAdapter();
+        list.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Log.d("TAG","row was clicked " + position);
+            }
+        });
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView nameTv;
+        TextView idTv;
+        CheckBox cb;
+
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+            nameTv = itemView.findViewById(R.id.listrow_name_tv);
+            idTv = itemView.findViewById(R.id.listrow_id_tv);
+            cb = itemView.findViewById(R.id.listrow_cb);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    listener.onItemClick(pos);
+                }
+            });
+        }
+    }
+
+    interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
+
+        OnItemClickListener listener;
+        public void setOnItemClickListener(OnItemClickListener listener){
+            this.listener = listener;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.student_list_row,parent,false);
+            MyViewHolder holder = new MyViewHolder(view,listener);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            Student student = data.get(position);
+            holder.nameTv.setText(student.getName());
+            holder.idTv.setText(student.getId());
+            holder.cb.setChecked(student.isFlag());
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+    }
+}
