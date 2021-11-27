6ecba150407266c5bee072798a62eb2b596ddc66
Voiko <nadavv10@walla.com>
2021-11-27 21:23:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/student_app_ex3/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.student_app_ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity  {
+    EditText nameEt;
+    EditText idEt;
+    EditText phone;
+    EditText address;
+    CheckBox cb;
+    Button edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameEt = findViewById(R.id.main_name_ET);
+        idEt = findViewById(R.id.main_id_ET);
+        phone = findViewById(R.id.main_phone_ET);
+        address = findViewById(R.id.main_address_ET);
+        cb = findViewById(R.id.main_checkBox);
+        edit = findViewById(R.id.main_BT_edit);
+
+    }
+
+
+}
