b9cd80ba9a93e68eb4b1572ee34831fd8d98efa8
Or Kravitz <imorkravitz@gmail.com>
2021-11-28 23:07:08 +0200

    updated

--- app/src/main/java/com/example/student_app_ex3/MainActivity.java
+++ app/src/main/java/com/example/student_app_ex3/MainActivity.java
@@ -2,34 +2,44 @@ package com.example.student_app_ex3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  {
-    EditText nameEt;
-    EditText idEt;
-    EditText phone;
-    EditText address;
+    TextView nameVt;
+    TextView idVt;
+    TextView phoneVt;
+    TextView addressVt;
     CheckBox cb;
     Button edit;
+    Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameEt = findViewById(R.id.main_name_ET);
-        idEt = findViewById(R.id.main_id_ET);
-        phone = findViewById(R.id.main_phone_ET);
-        address = findViewById(R.id.main_address_ET);
+        nameVt = findViewById(R.id.main_name_vt);
+        idVt = findViewById(R.id.main_id_vt);
+        phoneVt = findViewById(R.id.main_phone_vt);
+        addressVt = findViewById(R.id.main_address_vt);
         cb = findViewById(R.id.main_checkBox);
         edit = findViewById(R.id.main_BT_edit);
+        add = findViewById(R.id.main_btn);
 
     }
 
-//Testing
+    public void editStudent(View v){
+        Intent intent = new Intent(this, EditStudentActivit.class);
+        startActivity(intent);
+    }
+
+
+
 }
--- app/src/main/java/com/example/student_app_ex3/StudentListRvActivity.java
+++ app/src/main/java/com/example/student_app_ex3/StudentListRvActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
@@ -16,30 +19,41 @@ import java.util.List;
 import com.example.student_app_ex3.Model.Model;
 import com.example.student_app_ex3.Model.Student;
 
-public class StudentListRvAcivity extends AppCompatActivity {
+public class StudentListRvActivity extends AppCompatActivity {
 
     List<Student> data;
+    Button newStud;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list_rv_acivity);
 
         data = Model.instance.getAllStudents();
-
+        View setLayout = findViewById(R.id.layout_row);
         RecyclerView list = findViewById(R.id.studentlist_rv);
         list.setHasFixedSize(true);
-
         list.setLayoutManager(new LinearLayoutManager(this));
 
         MyAdapter adapter = new MyAdapter();
         list.setAdapter(adapter);
+        newStud = findViewById(R.id.main_btn);
 
         adapter.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
-                Log.d("TAG","row was clicked " + position);
+                Log.d("TAG","row was clicked " + position); // get the click on the list
             }
         });
+    }
+
+    public void setLayout(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void newStudent(View v){
+        Intent intent = new Intent(this, NewStudentActivity.class);
+        startActivity(intent);
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder{
@@ -52,11 +66,16 @@ public class StudentListRvAcivity extends AppCompatActivity {
             nameTv = itemView.findViewById(R.id.listrow_name_tv);
             idTv = itemView.findViewById(R.id.listrow_id_tv);
             cb = itemView.findViewById(R.id.listrow_cb);
+
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    int pos = getAdapterPosition();
+                    int pos = getAdapterPosition(); //the click on the item
                     listener.onItemClick(pos);
+                    Student student = data.get(pos);
+                    student.setFlag(cb.isChecked());
+                    Log.d("TAG","cb position" + pos);
+//                    setLayout(v);
                 }
             });
         }
